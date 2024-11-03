d99c625ea958b8550906ecec2a0d65365f20df22
maikerudev <michenbox@protonmail.com>
2024-11-03 21:40:54 +0000

    Add bootstrap css & js

--- header.php
+++ header.php
@@ -1,8 +1,13 @@
 <html>
-  <head> </head>
+  <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> <!-- Bootstrap css & js -->
+    <link rel="stylesheet" href="styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- https://getbootstrap.com/docs/5.3/getting-started/introduction/#viewport-meta -->
+</head> 
   <body>
-    <h1>Template</h1>
-
-    <p>This is a blank template for a web page.</p>
-  </body>
-</html>
+    <div id="header">
+    <h1>Cara's Art Shop</h1>
+    </div> 
+    <div class="card" style="width: 18rem">
+      <img src="./imgs/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg">
+    </div>
--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 
 <?php 
 
-include_once 'header.php'; 
+include_once 'header.php';  // This has the inital body and html tags.
 include_once 'sqlConfig_local.php';
 include_once 'sqlLibrary.php';
 
 
-echo "This is the main file.";
-
+echo `<div id="header"><p>This is the main file.</p></div>`;
 ?>
+<p> hello world!</p>
+  </body>
+</html>
+
